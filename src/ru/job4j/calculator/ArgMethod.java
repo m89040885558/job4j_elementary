a20778022cdf1a7ac2e4889a74ddc5637ca2284c
m89040885558 <m89040885558@yandex.ru>
2021-07-08 19:55:54 +0300

    9.2. Checkstyle [#4123]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -2,17 +2,16 @@ package ru.job4j.calculator;
 
 public class ArgMethod {
     public static void hello(String name) {
-        System.out.println("Hello, " + name);
-    }
+        System.out.println("Hello, " + name); }
+
     public static void hello(String name, int age) {
-        System.out.println("Hello, " + name + ", age = " + age);
-    }
+        System.out.println("Hello, " + name + ", age = " + age); }
+
     public static void hello(int age) {
-        System.out.println("age = " + age);
-    }
+        System.out.println("age = " + age); }
+
     public static void hello(int age, String name) {
-        System.out.println("age = " + age + ",Hello, " + name);
-    }
+        System.out.println("age = " + age + ",Hello, " + name); }
 
     public static void main(String[] args) {
         String name = "Roman Samorodskii";
--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -2,10 +2,11 @@ package ru.job4j.condition;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = Math.sqrt(Math.pow((x2-x1), 2)+Math.pow((y2-y1), 2) );
+        double rsl = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
         return rsl;
 
     }
+
     public static void main(String[] args) {
         double result = Point.distance(0, 0, 2, 0);
         double result1 = Point.distance(3, 5, 2, 0);
