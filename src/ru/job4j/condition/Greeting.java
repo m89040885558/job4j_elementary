7b2c8a1bdb6f1693e5268b5059215de98d51c93c
m89040885558 <m89040885558@yandex.ru>
2021-07-09 18:42:24 +0300

    9.1. String [#237407 #175517]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -2,8 +2,12 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-        String idea = "I like Java!" + " But I am a newbie. ";
+        String idea = "I like Java!";
+        System.out.println(idea);
+
+        idea += " But I am a newbie. ";
         int year = 2021;
-        System.out.println(idea + year + " year ");
+        idea += year;
+        System.out.println(idea);
     }
 }
