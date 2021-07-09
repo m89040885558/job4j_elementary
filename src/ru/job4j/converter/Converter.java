f3339eb89c5b705b54bb7850bfe98e08b895bd25
m89040885558 <m89040885558@yandex.ru>
2021-07-09 20:29:09 +0300

    0. Что такое тестирование [#159431]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -17,5 +17,17 @@ public class Converter {
         float dollar = Converter.rubleToDollar(140);
         System.out.println("140 rubles are " + euro + " euro.");
         System.out.println("140 rubles are " + dollar + " dollar.");
-    }
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        float in1 = 140;
+        float expected2 = 2.3333333F;
+        float out1 = Converter.rubleToDollar(in1);
+        boolean passed1 = expected2 == out1;
+        System.out.println("140 rubles are 2.3333333. Test result : " + passed1);
+      }
+
 }
+
