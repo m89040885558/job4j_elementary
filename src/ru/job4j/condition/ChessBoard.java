f6e06e64095368efecd6fc5d2e6978c4b5cb29c6
m89040885558 <m89040885558@yandex.ru>
2021-07-12 18:13:41 +0300

    4.3.6. Math.abs [#373616 #176737]

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -7,14 +7,13 @@ public class ChessBoard {
         if (x1 < 0 || x2 < 0 || y1 < 0 || y2 < 0 || x1 > 7 || x2 > 7 || y1 > 7 || y2 > 7) {
             return rsl;
         }
-            if (x1 - x2 == Math.abs(y1 - y2)) {
+            if (Math.abs(x1 - x2) == Math.abs(y1 - y2)) {
                 rsl = Math.abs(x1 - x2);
         }
         return rsl;
     }
 
     public static void main(String[] args) {
-
         System.out.println(way(2, 6, 4,  10));
     }
 }
