ad3896b81606f7a981ad346c9579cb9ce858287c
m89040885558 <m89040885558@yandex.ru>
2021-07-16 07:31:47 +0300

    5.0. Цикл for. Определение. [#3572 #177695]

--- src/ru/job4j/loop/LoopFor.java
+++ src/ru/job4j/loop/LoopFor.java
@@ -2,8 +2,9 @@ package ru.job4j.loop;
 
 public class LoopFor {
     public static void main(String[] args) {
-        for (i = 5; result = i <= 10; i++) {
+        for (int i = 5; i <= 10; i++) {
             System.out.println(i);
+
         }
     }
 }
