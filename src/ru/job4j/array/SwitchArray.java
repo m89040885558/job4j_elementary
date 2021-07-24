8782fd6f6eaf64b5f4f92a4c92d4eea9460ed430
m89040885558 <m89040885558@yandex.ru>
2021-07-24 11:44:23 +0300

    6.2.1. Выход за границу массива. [#226794 #181023]

--- src/ru/job4j/array/SwitchArray.java
+++ src/ru/job4j/array/SwitchArray.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class SwitchArray {
 
-    public static int[] swapBorder(int[] array) {
+        public static int[] swapBorder(int[] array) {
         int temp = array[0];
         array[0] = array[array.length - 1];
         array[array.length - 1] = temp;
