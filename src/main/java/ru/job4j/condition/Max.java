3ec6bf8a132292b1c9b82bc4546d304fef66ee79
m89040885558 <m89040885558@yandex.ru>
2021-09-03 03:17:06 +0300

    3. Перегрузить метод max для трех чисел [#122644]

---
+++ src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,16 @@
+package main.java.ru.job4j.condition;
+
+public class Max {
+
+    /*
+    public static int max(int left, int right) {
+        return left >= right ? left : right;
+    }
+*/
+    public static int max(int a, int b) {
+        return a >= b ? a : b;
+    }
+/*
+    public static void main(String[] args) {max(10, 9);}
+    */
+}
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -4,7 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MaxTest {
-    @Test
+
+    /* @Test
     public void whenMax1To2Then2() {
         int left = 10;
         int right = 9;
@@ -30,5 +31,34 @@ public class MaxTest {
         int expected = 10;
         Assert.assertEquals(result, expected);
     }
+*/
+    @Test
+    public void whenMax3To2To3() {
+        int a = 3;
+        int b = 2;
+        int result = Max.max(a, b);
+        int expected = 3;
+        Assert.assertEquals(result, expected);
+    }
 
+    @Test
+    public void whenMax3To2To5Then5() {
+        int a = 3;
+        int b = 2;
+        int c = 5;
+        int result = Max.max(a, b, c);
+        int expected = 5;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax3To2To5To9Then9() {
+        int a = 3;
+        int b = 2;
+        int c = 5;
+        int d = 9;
+        int result = Max.max(a, b, c, d);
+        int expected = 9;
+        Assert.assertEquals(result, expected);
+    }
 }
